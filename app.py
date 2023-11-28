7e30a4474dcee2f49e87b8b4da4787235de0fb97
Oussamcsc <abouyahia1o@alma.edu>
2023-11-27 21:38:58 -0500

    Added a services route, also an email to the contact page

--- app.py
+++ app.py
@@ -6,19 +6,25 @@ app = Flask(__name__)
 # Home page route
 @app.route('/')
 def index():
-    return 'Hello, Dexter! This is a Flask app.'
+    return 'Hello, Dexter! This is an updated Flask app.'
 
 
-# About page route
+# New Route: Services page
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
+# Updated About page route
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About Us')
 
 
 # Contact page route
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', email='lian@ryan.com')
 
 
 if __name__ == '__main__':
