6a1b7d7cbb6dfcdc21887d3b3af6d4b6dcba07d5
Oussamcsc <abouyahia1o@alma.edu>
2023-11-27 21:07:42 -0500

    Added a contact html file

--- app.py
+++ app.py
@@ -6,13 +6,19 @@ app = Flask(__name__)
 # Home page route
 @app.route('/')
 def index():
-    return 'Hello Dr. Dexter! This is a Flask app.'
+    return 'Hello, Dexter! This is a Flask app.'
 
 
 # About page route
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+# Contact page route
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
